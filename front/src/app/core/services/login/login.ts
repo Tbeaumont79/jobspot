0b69a37d48598dbd43258c1665a1d7952362bcf3
tbeaumont79 <tbeaumont79@icloud.com>
2025-07-09 18:09:50 +0200

    feat: working on login service

---
+++ front/src/app/core/services/login/login.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { User } from '../../shared/types/user';
+import { environment } from '../../../../environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class Login {
+  private http = inject(HttpClient);
+
+  login(email: string, password: string) {
+    return this.http.post<User>(`${environment.apiUrl}/auth`, {
+        email,
+        password,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+  }
+}
