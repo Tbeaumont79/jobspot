2af517a3780a9c103a6e4702e93c79481ddb5089
tbeaumont79 <tbeaumont79@icloud.com>
2025-07-12 16:52:37 +0200

    feat: offer now display job offer with cards

--- back/src/DataFixtures/OfferFixtures.php
+++ back/src/DataFixtures/OfferFixtures.php
@@ -33,6 +33,25 @@ class OfferFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($offer);
         $this->addReference('main-offer', $offer);
         $manager->flush();
+
+        $offer2 = new Offer();
+        $offer2->setTitle('Software Engineer 2');
+        $offer2->setDescription('We are looking for a software engineer 2 with 5 years of experience in PHP, Symfony, Laravel, React, Vue, Angular, Node, Python, Java, C#');
+        $offer2->setContractType('CDI');
+        $offer2->setSalary('100000');
+        $offer2->setCreatedAt(new \DateTimeImmutable('now'));
+        $offer2->setStatus(StatusEnum::PENDING);
+        $offer2->setCompany($company);
+
+        $tagNames2 = ['php', 'symfony', 'laravel', 'react', 'vue', 'angular', 'node', 'python', 'java', 'c#', 'c++', 'c'];
+        foreach ($tagNames2 as $tagName) {
+            $tag = $this->getReference('tag-' . $tagName, Tag::class);
+            $offer2->addTag($tag);
+        }
+        $manager->persist($offer2);
+        $this->addReference('main-offer-2', $offer2);
+        $manager->flush();
+
     }
 
     public function getDependencies(): array
--- front/src/app/pages/offer/offer.ts
+++ front/src/app/pages/offer/offer.ts
@@ -1,20 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { Offer as OfferService } from '../../core/services/offer/offer';
 import { OfferCard } from '../../components/offer-card/offer-card';
-import { OfferType } from '../../core/shared/types/offer';
+import { AsyncPipe } from '@angular/common';
+
 @Component({
   selector: 'app-offer',
-  imports: [OfferCard],
+  imports: [OfferCard, AsyncPipe],
   templateUrl: './offer.html',
 })
 export class Offer {
   private readonly offerService = inject(OfferService);
   offers$ = this.offerService.getOffers();
-  offers: OfferType[] = [];
-
-  ngOnInit() {
-    this.offers$.subscribe((offers) => {
-      this.offers = offers;
-    });
-  }
 }
