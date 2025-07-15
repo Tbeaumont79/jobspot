740e514f8d8d63639f03e10d30063e3f1bc68d36
tbeaumont79 <tbeaumont79@icloud.com>
2025-07-15 10:51:53 +0200

    feat: update home page template depending on the user roles

--- front/src/app/pages/home/home.ts
+++ front/src/app/pages/home/home.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthStateService } from '../../core/services/auth/auth-state';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   imports: [RouterLink],
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  private readonly authStateService = inject(AuthStateService);
+  private readonly authState$ = this.authStateService.authState$;
+  public user = this.authState$.value.user;
+}
